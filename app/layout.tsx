7757c1a027f21205a11dd5f284e3b59613b38a6c
Antoine Lafarge <alafarge@ankama.com>
2026-04-02 18:53:07 +0200

    Replace tailwind css to bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased background-black`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -100,8 +100,6 @@ export default function Home() {
 
       for await (const result of await chat({ model, messages: messages.slice(-MAX_CHAT_MESSAGES_COUNT).concat({ role: 'user', content: prompt }) })) {
         if (result.message?.content) {
-          console.log(result.message.content);
-
           response += result.message.content;
 
           addMessages(message, { role: "assistant", content: response });
@@ -141,48 +139,68 @@ export default function Home() {
   });
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col w-full items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Ollama-ui
-          </h1>
+    <>
+      <main className="flex-grow-1">
+
+        <div className="container">
           {
             messages.length
-              ? messages.map(({ role, content }, i) => <div key={i} className={`${role === 'assistant' ? '' : 'p-2 rounded-sm bg-gray-800'}`}>
-                <pre className="text-wrap">{content}</pre>
-              </div>)
+              ? messages.map(({ role, content }, i) => <>
+                <div className="row" key={i}>
+                  <div className={`col ${role === 'assistant' ? '' : 'p-2 rounded-sm bg-gray-800'}`}>
+                    <pre className="text-wrap">{content}</pre>
+                  </div>
+                </div>
+              </>)
               : undefined
           }
           {
             thinking
-              ? <ThinkingAnimation />
+              ? <div className="col"><ThinkingAnimation /></div>
               : undefined
           }
-          <form className="w-full" onSubmit={(event) => { event.preventDefault(); if (userContent?.length) { exec(); } }}>
-            <div className="w-full mb-4 border border-default-medium rounded-base bg-neutral-secondary-medium shadow-xs">
-              <div className="flex items-center justify-between px-3 py-2 border-b border-default-medium">
-                <div className="flex flex-wrap items-center divide-default-medium sm:divide-x sm:rtl:divide-x-reverse">
-                  <div className="flex items-center space-x-1 rtl:space-x-reverse sm:pe-4">
-                    <input id="inline-checkbox" type="checkbox" checked={chatMode} className="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft" onChange={(event) => setChatMode(event.target.checked)} />
-                    <label htmlFor="inline-checkbox" className="select-none ms-2 text-sm font-medium text-heading">Chat</label>
-                    <select id="models" className="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" value={model} onChange={(ev) => setModel(ev.target.value)}>
-                      {models.map((model) => <option className="dark:bg-black" value={model} key={model}>{model}</option>
-                      )}
-                    </select>
-                    <input type="text" id="pullModel" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body" placeholder="Pull model" disabled={pullProgress !== 0} style={{ background: `linear-gradient(to right, #3b82f6 ${100 * pullProgress}%, transparent ${100 * pullProgress}%)` }} value={modelToPull} onChange={(ev) => setModelToPull(ev.target.value)} onKeyDown={(event) => event.key === 'Enter' && execPull()} />
-                  </div>
-                </div>
-              </div>
-              <div className="px-4 py-2 bg-neutral-secondary-medium rounded-b-base">
-                <label htmlFor="userContent" className="sr-only">Send</label>
-                <textarea id="userContent" rows={8} className="block w-full px-0 text-sm text-heading bg-neutral-secondary-medium border-0 focus:ring-0 placeholder:text-body" placeholder="Prompt" required onChange={(event) => setUserContent(event.target.value)} onKeyDown={(event) => event.key === 'Enter' && !event.shiftKey && userContent?.length && exec()} value={userContent}></textarea>
+        </div >
+      </main>
+
+      <footer className="navbar navbar-dark bg-dark mt-auto">
+        <div className="container-fluid justify-content-center">
+          <span className="navbar-text me-2">Ollama-UI</span>
+          <div className="input-group mb-3">
+            <div className="input-group-text">
+              <input className="form-check-input mt-0 me-2" id="dzadza" type="checkbox" checked={true} aria-label="Checkbox for following text input" />
+              <label htmlFor="dzadza">Chat</label>
+            </div>
+            <select className="form-select" id="inputGroupSelect02" value={1}>
+              <option>Choose...</option>
+            </select>
+            <input type="text" className="form-control w-50" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <button className="btn btn-primary" type="button" id="button-addon2"><i className="bi bi-play-fill"></i></button>
+          </div>
+        </div>
+      </footer>
+
+      <form className="w-full" onSubmit={(event) => { event.preventDefault(); if (userContent?.length) { exec(); } }}>
+        <div className="w-full mb-4 border border-default-medium rounded-base bg-neutral-secondary-medium shadow-xs">
+          <div className="flex items-center justify-between px-3 py-2 border-b border-default-medium">
+            <div className="flex flex-wrap items-center divide-default-medium sm:divide-x sm:rtl:divide-x-reverse">
+              <div className="flex items-center space-x-1 rtl:space-x-reverse sm:pe-4">
+                <input id="inline-checkbox" type="checkbox" checked={chatMode} className="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft" onChange={(event) => setChatMode(event.target.checked)} />
+                <label htmlFor="inline-checkbox" className="select-none ms-2 text-sm font-medium text-heading">Chat</label>
+                <select id="models" className="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" value={model} onChange={(ev) => setModel(ev.target.value)}>
+                  {models.map((model) => <option className="dark:bg-black" value={model} key={model}>{model}</option>
+                  )}
+                </select>
+                <input type="text" id="pullModel" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body" placeholder="Pull model" disabled={pullProgress !== 0} style={{ background: `linear-gradient(to right, #3b82f6 ${100 * pullProgress}%, transparent ${100 * pullProgress}%)` }} value={modelToPull} onChange={(ev) => setModelToPull(ev.target.value)} onKeyDown={(event) => event.key === 'Enter' && execPull()} />
               </div>
             </div>
-            <button type="submit" className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" disabled={thinking} style={{ cursor: thinking ? 'not-allowed' : 'pointer' }}>{thinking ? 'Thinking...' : 'Send'}</button>
-          </form>
+          </div>
+          <div className="px-4 py-2 bg-neutral-secondary-medium rounded-b-base">
+            <label htmlFor="userContent" className="sr-only">Send</label>
+            <textarea id="userContent" rows={8} className="block w-full px-0 text-sm text-heading bg-neutral-secondary-medium border-0 focus:ring-0 placeholder:text-body" placeholder="Prompt" required onChange={(event) => setUserContent(event.target.value)} onKeyDown={(event) => event.key === 'Enter' && !event.shiftKey && userContent?.length && exec()} value={userContent}></textarea>
+          </div>
         </div>
-      </main>
-    </div>
+        <button type="submit" className="btn btn-primary" disabled={thinking} style={{ cursor: thinking ? 'not-allowed' : 'pointer' }}>{thinking ? 'Thinking...' : 'Send'}</button>
+      </form>
+    </>
   );
 }
