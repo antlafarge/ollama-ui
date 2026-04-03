f47c9d08421e5eeb680d5b74fae975c00047e0a2
Antoine Lafarge <alafarge@ankama.com>
2026-04-03 18:56:28 +0200

    navbar fixed bottom

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
         </div >
       </main>
 
-      <footer className="navbar navbar-dark bg-dark mt-auto">
+      <footer className="fixed-bottom navbar navbar-dark bg-dark mt-auto">
         <div className="container-fluid justify-content-center">
           <div className="input-group mb-3">
             <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{model ?? 'Ollama-UI'}</button>
